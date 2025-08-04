6a3183345e04fbda26e662b35c3f11a1f537f43b
mdmuntasir27 <md.muntasir.haque@g.bracu.ac.bd>
2025-08-04 20:37:17 +0600

    third commit

--- dummyproject/dummyproject/settings.py
+++ dummyproject/dummyproject/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'dummyproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodummyprojectcodecamp',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
